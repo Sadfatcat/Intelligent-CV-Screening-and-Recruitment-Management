9980526e55b49599c7922f190a2160e3c5b7cb1b
TienDat <bi26032005@gmail.com>
2026-04-07 17:23:34 +0700

    fix

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,12 +1,13 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
+from datetime import datetime
 
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str = Field(index=True, unique=True)
     password_hash: str
-    role: str = "recruiter"
+    role: str = "candidate"
 
 
 class Job(SQLModel, table=True):
